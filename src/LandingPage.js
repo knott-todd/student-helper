2ffa8a62bdacad6c119d601913ab1f18bce17546
knott-todd <100491331+knott-todd@users.noreply.github.com>
2025-07-02 17:42:44 -0400

    feat: :lipstick: tweak landing page styling (hero image and cta button)

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
         <div className={`${styles.header}`}>
             <div className={`${styles.logo}`}>
                 {/* Logo */}
-                <img alt="The Student Helper Logo" src="../public/logo512.png" />
+                <img style={{height: 32}} alt="The Student Helper Logo" src="/logo192.png" />
                 {/* Logo text */}
                 <p>The Student Helper</p>
             </div>
@@ -18,18 +18,23 @@ const LandingPage = () => {
 
         <div className={`${styles.landingView}`}>
 
-            {/* Fear less tag line display bold xl */}
-            <h1 className="display-xl-bold">FEAR LESS</h1>
+            <div className={`${styles.leftContent}`}>
 
-            {/* Sub head description body lg mt-16 */}
-            <p className="body-lg mt-md">With smart, personalized CXC practice</p>
+                {/* Fear less tag line display bold xl */}
+                <h1 className="display-xl-bold">FEAR LESS</h1>
 
-            {/* CTA button mt-64 */}
-            <PrimaryButton className="mt-lg" text="Start Practicing For Free" onClick={() => {}} />
+                {/* Sub head description body lg mt-16 */}
+                <p style={{maxWidth: 500}} className="body-lg mt-md">With CXC exam practice, syllabus mastery tracking, and personalized coaching to build real exam confidence.</p>
 
-            {/* Other users body sm mt-16 */}
-            <p className="body-sm mt-md">Trusted by 1,100+ students across the Caribbean to build exam confidence.</p>
+                {/* CTA button mt-64 */}
+                <PrimaryButton className="mt-lg" text="Start Practicing For Free" onClick={() => {}} />
 
+                {/* Other users body sm mt-16 */}
+                <p className="body-sm mt-md">Trusted by 1,100+ students across the Caribbean to build exam confidence.</p>
+
+            </div>
+
+            <img className="gradient-image" style={{height: '80%' , boxShadow: '0px 0px 41px -4px rgba(253, 157, 11,0.4),0px 0px 9px -3px rgba(253, 157, 11,0.6)', borderRadius: 10}} src="/superhero-with-bg.png" alt="Student Helper Superhero" />
         </div>
         </>
     )
