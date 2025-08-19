f5b9370d545932eeca3038a14412d1ec6567b837
knott-todd <100491331+knott-todd@users.noreply.github.com>
2025-08-19 15:57:47 -0400

    refactor: :art: convert Navbar to tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,22 @@
-import { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import {AppContext} from "../AppContext";
 import '../CSS/Navbar.css'
 import '../CSS/global.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useUser } from "@/features/auth/UserContext";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const Navbar = () => {
 
     const location = useLocation();
     const nav = useNavigate();
 
-    const global = useContext(AppContext);
+    // const { currExam } = useContext(AppContext) || { currExam: 1 };
+    const currExam = 1;
+    const { user, loading, error } = useUser();
 
     const [currSection, setCurrSection] = useState(window.location.pathname);
-    const [currPage, setCurrPage] = useState(window.location.pathname);
-    const [prevPage, setPrevPage] = useState("");
 
     useEffect(() => {
 
@@ -23,18 +25,19 @@ const Navbar = () => {
     }, [location])
 
     return (
+        loading ? <div>Loading user data...</div> :
         <div className="navbar">
             <nav>
-                <div style={{display: (global.userID && global.currExam ? "inline-flex" : "none"), alignItems: "center"}}>
+                <div style={{display: (user?.user_id && currExam ? "inline-flex" : "none"), alignItems: "center"}}>
                     <a className="nav-back" onClick={() => nav(-1)}><FontAwesomeIcon icon="arrow-left" /></a>
                     
                     <NavLink to='/tasks' currSection={currSection} onChange={setCurrSection} icon="list-check" />
                     <NavLink to="/test" currSection={currSection} onChange={setCurrSection} icon="file-pen" />
                     <NavLink to="/track" currSection={currSection} onChange={setCurrSection} icon="bars-progress" />
-                    {global.userID === 150 ? <NavLink to="/insert_objective" currSection={currSection} onChange={setCurrSection} text="Insert" icon="folder-plus" /> : ""}
+                    {user?.user_id === '150' ? <NavLink to="/insert_objective" currSection={currSection} onChange={setCurrSection} text="Insert" icon="folder-plus" /> : ""}
                 </div>
                 <div style={{display: "inline-block"}}>
-                    <NavLink to="/sign_in" currSection={currSection} onChange={setCurrSection} text={global.userID ? "Profile" : "Sign In"} icon="user" />
+                    <NavLink to="/sign_in" currSection={currSection} onChange={setCurrSection} text={user?.user_id ? "Profile" : "Sign In"} icon="user" />
                 </div>
                 
             </nav>
@@ -42,10 +45,18 @@ const Navbar = () => {
     )
 }
 
-const NavLink = (props) => {
+interface NavLinkProps {
+    to: string;
+    currSection: string;
+    onChange: (pathname: string) => void;
+    icon?: IconProp;
+    text?: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = (props) => {
 
     return (
-        <Link to={props.to} onClick={e => props.onChange(e.target.pathname)} className={props.currSection && props.currSection.substring(0, props.to.length) === props.to ? "current" : ""}>
+        <Link to={props.to} onClick={e => props.onChange((e.target as HTMLAnchorElement).pathname)} className={props.currSection && props.currSection.substring(0, props.to.length) === props.to ? "current" : ""}>
             {props.icon ? <FontAwesomeIcon icon={props.icon} /> : (props.text ? props.text : props.to.substring(1).charAt(0).toUpperCase() + props.to.slice(2))}
             <br />
             <p>{props.text ? props.text : props.to.substring(1).charAt(0).toUpperCase() + props.to.slice(2)}</p>
